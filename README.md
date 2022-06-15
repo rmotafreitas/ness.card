d6c429689a437f018acb3ca91369d5fc6ad90595
Ness.js <68559739+rmotafreitas@users.noreply.github.com>
2022-06-15 03:15:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Profile
+
+My profile card, based on [IchiiDev Repo](https://github.com/IchiiDev/profile)
+
+## Ressources
+
+<p>Copyright &copy Discord 2021, I do not own any of the Discord assets and these are hosted on Discord's website</p>
---
+++ assets/config.js
@@ -0,0 +1,41 @@
+const config = {
+  USERNAME: "Ness.js",
+
+  PICTURE: "assets/img/profile_picture.gif",
+  HEADER: "assets/img/header.gif",
+
+  DESCRIPTION:
+    "Hi, I'm Ness, a 17 years old young Portuguese programmer and gamer in his free time.",
+
+  BADGES: [
+    { id: "NodeJS developer", image: "assets/img/nodebadge.png" },
+    { id: "ReactJS developer", image: "assets/img/reactbadge.png" },
+    { id: "MongoDB User", image: "assets/img/mongobadge.png" },
+  ],
+
+  PROJECTS: [
+    {
+      id: "project-1",
+      image: "assets/img/kaori.gif",
+      link: "https://bot-kaori-node.herokuapp.com/",
+      name: "Kaori Discord Bot",
+    },
+    {
+      id: "project-2",
+      image: "assets/img/notes.png",
+      link: "#",
+      name: "Note's Home Mobile App",
+    },
+    {
+      id: "project-3",
+      image: "assets/img/lofi.gif",
+      link: "#",
+      name: "Lofi Pomodoro",
+    },
+  ],
+
+  SOCIALS: [
+    { class_name: "fab fa-github", link: "https://github.com/rmotafreitas" },
+    { class_name: "fab fa-twitter", link: "https://twitter.com/rmotafreitas" },
+  ],
+};
